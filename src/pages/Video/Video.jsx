7bc3411fdb5646667a159f87effdf3f46b031a21
sumit-0-0-1 <sumit.email.dev@gmail.com>
2022-07-25 08:58:55 +0530

    refactored code

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,20 +1,13 @@
 import './Video.css';
 import VideoCard from '../../components/Card/VideoCard/VideoCard.jsx';
 
-
-
 import { useParams } from "react-router-dom";
 import { usePlaylist } from "../../contexts/playlistContext";
 import { useState } from "react";
 import YouTube from "react-youtube";
 
 
-
-
-
 const Video = () => {
-
-
   const [sizeOfWindow, setSizeOfWindow] = useState(window.innerWidth);
   const { playlistDispatch, videoData } = usePlaylist();
   const { videoId } = useParams();
@@ -42,11 +35,10 @@ const Video = () => {
   };
 
  
-
     return (
         <>
             {/* main section */}
-
+           
             <section className="video-page-main-section">
                 {/* section-1 */}
 
@@ -97,22 +89,21 @@ const Video = () => {
                         <span>
                             <i className="fa fa-th-list" aria-hidden="true" /> Add To Playlist
                         </span>{" "}
-                        <span className="text-float-right">12k View | 2hr ago</span>
+                       
                     </p>
-                    <h4 id='text-left-side'>Description </h4>
-                    <h3 id='text-left-side'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, et. Molestias, dignissimos! Omnis ipsam cupiditate incidunt laudantium ipsum doloribus exercitationem necessitatibus non, consectetur, aliquid id, officiis explicabo? Adipisci, maiores quaerat!
-                    </h3>
+                    <hr />
+                    <h3 id='text-left-side'>Must watch </h3>
+                   
                 </section>
 
-
+              
 
 
                 {/* end of section-1 */}
                 {/* section-2 */}
+               
                 <section className="video-page-section-2">
-                    <h3 id='text-left-side'>Must Watch</h3>
-
+                 
                     {
                         videoData.map((video) => {
                             return <VideoCard id={video.id} thumbnail={video.image} title={video.title} views={video.views} xDaysAgo={video.date} video = {video} type= {"ADD_TO_WATCHLATER"} />
